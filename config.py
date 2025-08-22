c25016b4d3f35dbf18aa122260379f5097f56f07
ANTI <103268389+ANTIoffz@users.noreply.github.com>
2025-08-22 19:38:25 +0700

    Upload

---
+++ config.py
@@ -0,0 +1,25 @@
+from dataclasses import dataclass
+from typing import Tuple
+
+@dataclass
+class ZoneConfig:
+    name: str
+    image_pos: Tuple[Tuple[int, int, int, int], ...]
+    save_file: str
+    bot_token: str
+    chat_id: str
+    interval: int = 600
+    ignored_authors: Tuple[int, ...] = ()
+
+
+zones = [
+    ZoneConfig(
+        name="Арт 1",  # Название зоны (любое для удобства)
+        image_pos=((0, 0, 100, 100), (100, 100, 200, 200)),  # Координаты: (Tile X, Tile Y, Pixel X, Pixel Y)
+        save_file="zone1.png",  # Файл для сохранения текущего состояния зоны
+        bot_token="...",  # Токен Telegram-бота (получить у @BotFather)
+        chat_id="...",  # ID чата или канала для уведомлений
+        ignored_authors=(),  # Игнорируемые авторы (их ID можно узнать через API)
+        interval=600  # Интервал проверки в секундах (по умолчанию 600 = 10 минут)
+    ),
+]
